12871357f326fc9a0ad0d6f03ea2f6d520c823ec
Suraj Giri <sg746592@gmail.com>
2023-09-02 16:49:37 +0545

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/khalti_api", async (req, res) => {
+  const payload = req.body;
+  const khaltiResponse = await axios.post(
+    "https://a.khalti.com/api/v2/epayment/initiate/",
+    payload,
+    {
+      headers: {
+        Authorization: `Key ${process.env.KHALTI_SECRET_KEY}`,
+      },
+    }
+  );
+
+  if (khaltiResponse) {
+    res.json({
+      success: true,
+      data: khaltiResponse?.data,
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "some",
+    });
+  }
+  console.log(khaltiResponse);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
